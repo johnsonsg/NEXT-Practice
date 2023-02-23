8c40eba7fa3dbad0ec2d9326ca98103a43f22eaf
Shawn Johnson <johnson.sg@gmail.com>
2023-02-22 23:15:44 -0500

    data fetching with parmas for SSG and Preparing paths

--- my-app/src/pages/[meetupId]/index.js
+++ my-app/src/pages/[meetupId]/index.js
@@ -14,4 +14,45 @@ function MeetupDetails() {
   )
 }
 
+// Note: getStaticProps and getStaticPaths only runs during build time. And only runs on the developerserver side.
+// So, we only see it in the developer terminal, and not in the browser.
+
+// must include when using getStaticProps
+export async function getStaticPaths() {
+  return {
+    fallback: false, // false define some paths - true define all paths
+    paths: [
+      {
+        params: {
+          meetupId: 'm1'
+        }
+      },
+      {
+        params: {
+          meetupId: 'm2'
+        }
+      }
+    ]
+  }
+}
+
+// getStaticProps
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId
+  // console.log(meetupId)
+
+  return {
+    props: {
+      meetupData: {
+        image: `https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153`,
+        id: 'm1',
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup'
+      }
+    }
+  }
+}
+
 export default MeetupDetails
