768faa19487cdda7b5f4671f517523c1a10daae8
Shawn Johnson <johnson.sg@gmail.com>
2023-02-25 15:51:11 -0500

    Getting Meetup Details Data & Preparing Pages

--- my-app/src/pages/[meetupId]/index.js
+++ my-app/src/pages/[meetupId]/index.js
@@ -1,14 +1,15 @@
 import { Fragment } from 'react'
+import { MongoClient, ObjectId } from 'mongodb'
 import MeetupDetail from '@/components/meetups/meetupDetail'
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <Fragment>
       <MeetupDetail
-        image={`https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153`}
-        title='First Meetup'
-        address='Some Street 5, Some City'
-        description='This is a first meetup'
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
       />
     </Fragment>
   )
@@ -19,20 +20,34 @@ function MeetupDetails() {
 
 // must include when using getStaticProps
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://johnsonsg:6LOAXi8KulHpceIa@cluster0.wvnjnk4.mongodb.net/meetups?retryWrites=true&w=majority'
+  )
+  const db = client.db()
+
+  const meetupsCollection = db.collection('meetups')
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray()
+
+  client.close()
+
   return {
     fallback: false, // false define some paths - true define all paths
-    paths: [
-      {
-        params: {
-          meetupId: 'm1'
-        }
-      },
-      {
-        params: {
-          meetupId: 'm2'
-        }
-      }
-    ]
+    paths: meetups.map(meetup => ({
+      params: { meetupId: meetup._id.toString() }
+    }))
+    // [
+    //   ({
+    //     params: {
+    //       meetupId: 'm1'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       meetupId: 'm2'
+    //     }
+    //   })
+    // ]
   }
 }
 
@@ -42,14 +57,27 @@ export async function getStaticProps(context) {
   const meetupId = context.params.meetupId
   // console.log(meetupId)
 
+  const client = await MongoClient.connect(
+    'mongodb+srv://johnsonsg:6LOAXi8KulHpceIa@cluster0.wvnjnk4.mongodb.net/meetups?retryWrites=true&w=majority'
+  )
+  const db = client.db()
+
+  const meetupsCollection = db.collection('meetups')
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId)
+  })
+
+  client.close()
+
   return {
     props: {
       meetupData: {
-        image: `https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153`,
-        id: 'm1',
-        title: 'First Meetup',
-        address: 'Some Street 5, Some City',
-        description: 'This is a first meetup'
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description
       }
     }
   }
